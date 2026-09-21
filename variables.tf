caaf97ef86f25a72080228dcff1068c93ce49bfd
Mukul Sharma <185355609+MukulSha-afk@users.noreply.github.com>
2026-09-21 11:22:51 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances"
+  type        = number
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "subnet_cidrs" {
+  description = "Subnet CIDRs"
+  type        = list(string)
+}
